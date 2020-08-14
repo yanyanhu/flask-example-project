89c0f74944520cad8add3776ced47af126eeb14a
yanyanhu <yanyan.hu@moht.com.sg>
2020-08-14 05:05:32 +0000

    Rename app/ to myapp/

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'myapp.sqlite'),
     )
 
     if test_config is None:
@@ -27,6 +27,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
+        print(os.environ['APP_SETTINGS'])
         return 'Hello, World!'
 
     return app
